32d3ac406bb50b9a8b9f794f6ca4fb52fbed877c
Illya <illya199614@gmail.com>
2021-06-20 01:06:09 +0300

    Set up form (data and buttons) on personal page

--- src/app/customer/personal-page/personal-page.component.ts
+++ src/app/customer/personal-page/personal-page.component.ts
@@ -9,23 +9,23 @@ class Client{
 }
 
 export interface PeriodicElement {
+  position:number;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  date: string;
+  time: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Перманент брів', date: "15/1/2021", time: '14:00'},
+  {position: 2, name: 'Перманент стрілка (корекція)', date: "20/2/2021", time: '11:00'},
+  {position: 3, name: 'Перманент губ', date: "15/2/2021", time: '13:30'},
+  {position: 4, name: 'Фарбування вій + ботокс', date: "6/3/2021", time: '17:00'},
+  {position: 5, name: 'Перманент міжвійка (корекція)', date: "19/3/2021", time: '11:30'},
+  {position: 6, name: 'Перманент губ (корекція)', date: "24/4/2021", time: '15:00'},
+  {position: 7, name: 'Довготривала укладка брів', date: "31/5/2021", time: '18:00'},
+  {position: 8, name: 'Перманент міжвійка', date: "27/6/2021", time: '16:30'},
+  {position: 9, name: 'Перманент стрілка', date: "30/6/2021", time: '10:30'},
+  {position: 10, name: 'Ламінування вій', date:"8/7/2021", time: '19:00'},
 ];
 
 @Component({
@@ -39,13 +39,40 @@ export class PersonalPageComponent implements OnInit {
 
   formData:Client = new Client();
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['position', 'name', 'date', 'time', 'actions'];
   dataSource = ELEMENT_DATA;
+  checked = false;
+  isDisabled = true;
 
   ngOnInit(): void {
     this.formData.fullName = 'Марія';
     this.formData.instagramName = 'mariya_inst';
     this.formData.phoneNumber = '0978467373';
     this.formData.telegramChatId = 2664346812123;
+  }
+
+  changed(){
+    if(this.checked)
+    {
+      this.dataSource = [
+        {position: 1, name: 'Перманент міжвійка', date: "27/6/2021", time: '16:30'},
+        {position: 2, name: 'Перманент стрілка', date: "30/6/2021", time: '10:30'},
+        {position: 3, name: 'Ламінування вій', date:"8/7/2021", time: '19:00'}
+      ]
+    }
+    else{
+      this.dataSource = ELEMENT_DATA;
+    }
+  }
+
+  disabledCheck(index:number): boolean {
+    return (this.dataSource[index].date == "27/6/2021" ||
+           this.dataSource[index].date == "30/6/2021" ||
+           this.dataSource[index].date == "8/7/2021")
+     ? false : true;
+  }
+
+  openAcceptDialog() {
+    
   }
 }
